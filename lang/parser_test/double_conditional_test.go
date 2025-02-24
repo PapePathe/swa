cea96742649adb97cadb6e8dbe60e829363b4aef
Pathe SENE <pathe.sene@gmail.com>
2025-02-24 18:33:40 +0000

    Fix golangci-lint issue.
     the generated ast will be large enough and that golangci-lint
    will warn about duplicated code paths in the resulting ast.

---
+++ lang/parser_test/double_conditional_test.go
@@ -0,0 +1,85 @@
+package parser_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"swahili/lang/ast"
+	"swahili/lang/lexer"
+	"swahili/lang/parser"
+)
+
+func TestDualBranchConditional(t *testing.T) {
+	result := parser.Parse(lexer.Tokenize(`
+    ni(x>0) {
+      width = 100;
+      height = 100 + 400 - width;
+    } nii {
+    	width += 300;
+    	height += rand / 14;
+    }
+	`))
+
+	expected := ast.BlockStatement{
+		Body: []ast.Statement{
+			ast.ConditionalStatetement{
+				Condition: ast.BinaryExpression{
+					Left:     ast.SymbolExpression{Value: "x"},
+					Right:    ast.NumberExpression{Value: 0},
+					Operator: lexer.Token{Value: ">", Kind: 18},
+				},
+				Success: ast.BlockStatement{
+					Body: []ast.Statement{
+						ast.ExpressionStatement{
+							Exp: ast.AssignmentExpression{
+								Operator: lexer.Token{Value: "=", Kind: 9},
+								Assignee: ast.SymbolExpression{Value: "width"},
+								Value:    ast.NumberExpression{Value: 100},
+							},
+						},
+						ast.ExpressionStatement{
+							Exp: ast.AssignmentExpression{
+								Operator: lexer.Token{Value: "=", Kind: 9},
+								Assignee: ast.SymbolExpression{Value: "height"},
+								Value: ast.BinaryExpression{
+									Left: ast.BinaryExpression{
+										Left:     ast.NumberExpression{Value: 100},
+										Right:    ast.NumberExpression{Value: 400},
+										Operator: lexer.Token{Value: "+", Kind: 30},
+									},
+									Right:    ast.SymbolExpression{Value: "width"},
+									Operator: lexer.Token{Value: "-", Kind: 22},
+								},
+							},
+						},
+					},
+				},
+				Failure: ast.BlockStatement{
+					Body: []ast.Statement{
+						ast.ExpressionStatement{
+							Exp: ast.AssignmentExpression{
+								Operator: lexer.Token{Value: "+=", Kind: 31},
+								Assignee: ast.SymbolExpression{Value: "width"},
+								Value:    ast.NumberExpression{Value: 300},
+							},
+						},
+						ast.ExpressionStatement{
+							Exp: ast.AssignmentExpression{
+								Operator: lexer.Token{Value: "+=", Kind: 31},
+								Assignee: ast.SymbolExpression{Value: "height"},
+								Value: ast.BinaryExpression{
+									Left:     ast.SymbolExpression{Value: "rand"},
+									Right:    ast.NumberExpression{Value: 14},
+									Operator: lexer.Token{Value: "/", Kind: 15},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	assert.Equal(t, expected, result)
+}
