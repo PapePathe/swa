9cf039604556d91f044cdc782b25ccf2a7a9f69e
Pathe <pathe.sene@gmail.com>
2025-09-14 20:39:30 +0000

    chore: extract compiler ctx def to file

--- lang/ast/ast.go
+++ lang/ast/ast.go
@@ -13,17 +13,3 @@ type Statement interface {
 type Expression interface {
 	CompileLLVM(ctx *CompilerCtx) (error, *llvm.Value)
 }
-
-type StructSymbolTableEntry struct {
-	LLVMType llvm.Type
-	Metadata StructDeclarationStatement
-}
-
-type CompilerCtx struct {
-	Context           *llvm.Context
-	Builder           *llvm.Builder
-	Module            *llvm.Module
-	SymbolTable       map[string]llvm.Value
-	StructSymbolTable map[string]StructSymbolTableEntry
-	FuncSymbolTable   map[string]llvm.Type
-}
