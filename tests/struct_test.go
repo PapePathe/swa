587a0e0809a124399d575791daccc6e12c97f9ec
Pathe <pathe.sene@gmail.com>
2025-09-20 09:20:44 +0000

    chore: adjust test file

--- tests/struct_test.go
+++ tests/struct_test.go
@@ -18,12 +18,12 @@ declare i32 @printf(ptr, ...)
 define i32 @main() {
 func-body:
   %0 = alloca %Engineer, align 8
-  %Age = getelementptr inbounds %Engineer, ptr %0, i32 0, i32 0
-  store i32 40, ptr %Age, align 4
-  %Name = getelementptr inbounds %Engineer, ptr %0, i32 0, i32 1
-  store [6 x i8] c"Pathe\00", ptr %Name, align 1
-  %TechStack = getelementptr inbounds %Engineer, ptr %0, i32 0, i32 2
-  store [15 x i8] c"Ruby, Rust, Go\00", ptr %TechStack, align 1
+  %1 = getelementptr inbounds %Engineer, ptr %0, i32 0, i32 0
+  store i32 40, ptr %1, align 4
+  %2 = getelementptr inbounds %Engineer, ptr %0, i32 0, i32 1
+  store [6 x i8] c"Pathe\00", ptr %2, align 1
+  %3 = getelementptr inbounds %Engineer, ptr %0, i32 0, i32 2
+  store [15 x i8] c"Ruby, Rust, Go\00", ptr %3, align 1
   ret i32 0
 }
 `
@@ -49,18 +49,18 @@ declare i32 @printf(ptr, ...)
 define i32 @main() {
 func-body:
   %0 = alloca %Engineer, align 8
-  %Age = getelementptr inbounds %Engineer, ptr %0, i32 0, i32 0
-  store i32 40, ptr %Age, align 4
-  %Name = getelementptr inbounds %Engineer, ptr %0, i32 0, i32 1
-  store [6 x i8] c"Pathe\00", ptr %Name, align 1
-  %TechStack = getelementptr inbounds %Engineer, ptr %0, i32 0, i32 2
-  store [15 x i8] c"Ruby, Rust, Go\00", ptr %TechStack, align 1
   %1 = getelementptr inbounds %Engineer, ptr %0, i32 0, i32 0
-  %2 = alloca [15 x i8], align 1
-  store [15 x i8] c"Struct created\00", ptr %2, align 1
-  %3 = alloca [29 x i8], align 1
-  store [29 x i8] c"TODO display value of r1.Age\00", ptr %3, align 1
-  %4 = call i32 (ptr, ...) @printf(ptr %2, ptr %3)
+  store i32 40, ptr %1, align 4
+  %2 = getelementptr inbounds %Engineer, ptr %0, i32 0, i32 1
+  store [6 x i8] c"Pathe\00", ptr %2, align 1
+  %3 = getelementptr inbounds %Engineer, ptr %0, i32 0, i32 2
+  store [15 x i8] c"Ruby, Rust, Go\00", ptr %3, align 1
+  %4 = getelementptr inbounds %Engineer, ptr %0, i32 0, i32 0
+  %5 = alloca [15 x i8], align 1
+  store [15 x i8] c"Struct created\00", ptr %5, align 1
+  %6 = alloca [29 x i8], align 1
+  store [29 x i8] c"TODO display value of r1.Age\00", ptr %6, align 1
+  %7 = call i32 (ptr, ...) @printf(ptr %5, ptr %6)
   ret i32 100
 }
 `
@@ -109,10 +109,10 @@ declare i32 @printf(ptr, ...)
 define i32 @main() {
 func-body:
   %0 = alloca %Vec, align 8
-  %x = getelementptr inbounds %Vec, ptr %0, i32 0, i32 0
-  store i32 0, ptr %x, align 4
   %1 = getelementptr inbounds %Vec, ptr %0, i32 0, i32 0
-  store i32 100, ptr %1, align 4
+  store i32 0, ptr %1, align 4
+  %2 = getelementptr inbounds %Vec, ptr %0, i32 0, i32 0
+  store i32 100, ptr %2, align 4
   ret i32 0
 }
 `
