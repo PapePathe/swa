706afcd905af7ee67a553b8c3ef910aa70cfa7f2
pathe <pathe.sene@gmail.com>
2025-10-10 19:33:35 +0000

    dep: generate the output path

--- tests/main_test.go
+++ tests/main_test.go
@@ -6,23 +6,28 @@ import (
 	"os/exec"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
 type CompileRequest struct {
-	Folder         string
-	InputPath      string
-	OutputPath     string
-	ExpectedLLIR   string
+	Folder       string
+	InputPath    string
+	OutputPath   string
+	ExpectedLLIR string
+
+	// The expected output after the program is compiled
 	ExpectedOutput string
+
 	// The expected output when the program is executed on the host machine
 	ExpectedExecutionOutput string
 	T                       *testing.T
 }
 
-func (cr CompileRequest) Compile() error {
+func (cr *CompileRequest) Compile() error {
 	cr.T.Helper()
 
+	cr.OutputPath = uuid.New().String()
 	cmd := exec.Command("./swa", "compile", "-s", cr.InputPath, "-o", cr.OutputPath)
 
 	output, err := cmd.CombinedOutput()
@@ -32,7 +37,7 @@ func (cr CompileRequest) Compile() error {
 	return err
 }
 
-func (cr CompileRequest) RunProgram() error {
+func (cr *CompileRequest) RunProgram() error {
 	cr.T.Helper()
 
 	cmd := exec.Command(fmt.Sprintf("./%s.exe", cr.OutputPath))
@@ -49,34 +54,17 @@ func (cr CompileRequest) RunProgram() error {
 	return err
 }
 
-func (cr CompileRequest) AssertCompileAndExecute() {
+func (cr *CompileRequest) AssertCompileAndExecute() {
 	if err := cr.Compile(); err != nil {
 		cr.T.Fatalf("Compiler error (%s)", err)
 	}
-
-	//	cr.AssertGeneratedLLIR()
 
 	if err := cr.RunProgram(); err != nil {
 		cr.T.Fatalf("Runtime error (%s)", err)
 	}
 }
 
-func (cr CompileRequest) AssertGeneratedAssembly() {
-}
-
-func (cr CompileRequest) AssertGeneratedLLIR() {
-	cr.T.Helper()
-
-	actualStr, err := os.ReadFile(fmt.Sprintf("%s.ll", cr.OutputPath))
-	assert.NoError(cr.T, err)
-
-	expectedStr, err := os.ReadFile(cr.ExpectedLLIR)
-	assert.NoError(cr.T, err)
-
-	assert.Equal(cr.T, string(actualStr), string(expectedStr))
-}
-
-func (cr CompileRequest) Cleanup() {
+func (cr *CompileRequest) Cleanup() {
 	cr.T.Helper()
 
 	assert.NoError(cr.T, os.Remove(fmt.Sprintf("%s.ll", cr.OutputPath)))
--- tests/syntax_error_tests.go
+++ tests/syntax_error_tests.go
@@ -9,7 +9,6 @@ func TestMissingDialect(t *testing.T) {
 
 	req := CompileRequest{
 		InputPath:               "./examples/missing_dialect.swa",
-		OutputPath:              "55109834-f1de-425f-9e5a-562e8defd4d7",
 		ExpectedExecutionOutput: "you must define your dialect\n",
 		T:                       t,
 	}
