662675d0028e77f2e52e936c809daede00cbaa8c
pathe <pathe.sene@gmail.com>
2025-10-10 19:33:16 +0000

    dep: cleanup test files

--- tests/arrays_test.go
+++ tests/arrays_test.go
@@ -12,8 +12,6 @@ func TestArrays(t *testing.T) {
 	t.Run("French", func(t *testing.T) {
 		req := CompileRequest{
 			InputPath:      "./arrays/ok/source.french.swa",
-			ExpectedLLIR:   "./arrays/ok/source.french.ll",
-			OutputPath:     "474c1abf-c877-455b-b6b9-09bdbc9b3d47",
 			ExpectedOutput: "",
 			T:              t,
 		}
@@ -25,8 +23,6 @@ func TestArrays(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
 		req := CompileRequest{
 			InputPath:      "./arrays/ok/source.english.swa",
-			ExpectedLLIR:   "./arrays/ok/source.english.ll",
-			OutputPath:     "f32e0fc5-a990-491e-8c5b-89a787866f1a",
 			ExpectedOutput: "",
 			T:              t,
 		}
@@ -42,8 +38,6 @@ func TestArrayIndexOutOfBounds(t *testing.T) {
 	t.Run("French", func(t *testing.T) {
 		req := CompileRequest{
 			InputPath:      "./arrays/out-of-bounds/source.french.swa",
-			ExpectedLLIR:   "./arrays/out-of-bounds/source.french.ll",
-			OutputPath:     "78cd171d-69e5-44c9-84c1-85c3939d00a8",
 			ExpectedOutput: "Element at index (5) does not exist in array (tableau)\n",
 			T:              t,
 		}
@@ -54,8 +48,6 @@ func TestArrayIndexOutOfBounds(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
 		req := CompileRequest{
 			InputPath:      "./arrays/out-of-bounds/source.english.swa",
-			ExpectedLLIR:   "./arrays/out-of-bounds/source.english.ll",
-			OutputPath:     "66ebfb11-c40a-41d5-84c1-de0b710c9a88",
 			ExpectedOutput: "Element at index (5) does not exist in array (array)\n",
 			T:              t,
 		}
@@ -70,8 +62,6 @@ func TestArrayAccessWithPrefixExpression(t *testing.T) {
 	t.Run("French", func(t *testing.T) {
 		req := CompileRequest{
 			InputPath:      "./arrays/prefix-expression/source.french.swa",
-			ExpectedLLIR:   "./arrays/prefix-expression/source.french.ll",
-			OutputPath:     "a6fe34d3-83f8-4dc5-a03e-c786ce604da2",
 			ExpectedOutput: "Only numbers are supported as array index, current: ({{- MINUS MINUS} {4}})\n",
 			T:              t,
 		}
@@ -82,8 +72,6 @@ func TestArrayAccessWithPrefixExpression(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
 		req := CompileRequest{
 			InputPath:      "./arrays/prefix-expression/source.english.swa",
-			ExpectedLLIR:   "./arrays/prefix-expression/source.english.ll",
-			OutputPath:     "89e28b67-a4e4-4a45-855e-eb14ca790ba6",
 			ExpectedOutput: "Only numbers are supported as array index, current: ({{- MINUS MINUS} {4}})\n",
 			T:              t,
 		}
@@ -98,8 +86,6 @@ func TestArrayAccessWithSymbolExpression(t *testing.T) {
 	t.Run("French", func(t *testing.T) {
 		req := CompileRequest{
 			InputPath:      "./arrays/symbol-expression/source.french.swa",
-			ExpectedLLIR:   "./arrays/symbol-expression/source.french.ll",
-			OutputPath:     "31e357ba-aa48-4999-b437-289b3cf1cac4",
 			ExpectedOutput: "Only numbers are supported as array index, current: ({x})\n",
 			T:              t,
 		}
@@ -110,8 +96,6 @@ func TestArrayAccessWithSymbolExpression(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
 		req := CompileRequest{
 			InputPath:      "./arrays/symbol-expression/source.english.swa",
-			ExpectedLLIR:   "./arrays/symbol-expression/source.english.ll",
-			OutputPath:     "602748f7-8ced-4180-8247-f27c54524196",
 			ExpectedOutput: "Only numbers are supported as array index, current: ({x})\n",
 			T:              t,
 		}
--- tests/struct_test.go
+++ tests/struct_test.go
@@ -12,8 +12,6 @@ func TestStructWithUnknownType(t *testing.T) {
 	t.Run("French", func(t *testing.T) {
 		req := CompileRequest{
 			InputPath:      "./structs/unknown-property-type/source.french.swa",
-			ExpectedLLIR:   "./structs/unknown-property-type/source.french.ll",
-			OutputPath:     "5e9377cb-904d-484b-9b62-28e531340079",
 			ExpectedOutput: "struct proprerty type ({unknown_type}) not supported\n",
 			T:              t,
 		}
@@ -24,8 +22,6 @@ func TestStructWithUnknownType(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
 		req := CompileRequest{
 			InputPath:      "./structs/unknown-property-type/source.english.swa",
-			ExpectedLLIR:   "./structs/unknown-property-type/source.english.ll",
-			OutputPath:     "5e9377cb-904d-484b-9b62-28e531340079",
 			ExpectedOutput: "struct proprerty type ({unknown_type}) not supported\n",
 			T:              t,
 		}
@@ -39,10 +35,8 @@ func TestStructPropertyAssignment(t *testing.T) {
 
 	t.Run("French", func(t *testing.T) {
 		req := CompileRequest{
-			InputPath:    "./structs/assignment/source.french.swa",
-			ExpectedLLIR: "./structs/assignment/source.french.ll",
-			OutputPath:   "27a8a248-e3d8-4fd0-b99a-15c9160d30f5",
-			T:            t,
+			InputPath: "./structs/assignment/source.french.swa",
+			T:         t,
 		}
 
 		defer req.Cleanup()
@@ -52,10 +46,8 @@ func TestStructPropertyAssignment(t *testing.T) {
 
 	t.Run("English", func(t *testing.T) {
 		req := CompileRequest{
-			InputPath:    "./structs/assignment/source.english.swa",
-			ExpectedLLIR: "./structs/assignment/source.english.ll",
-			OutputPath:   "27a8a248-e3d8-4fd0-b99a-15c9160d30f5",
-			T:            t,
+			InputPath: "./structs/assignment/source.english.swa",
+			T:         t,
 		}
 
 		defer req.Cleanup()
@@ -99,10 +91,8 @@ func TestStructPropertyInReturnExpression(t *testing.T) {
 
 	t.Run("English", func(t *testing.T) {
 		req := CompileRequest{
-			InputPath:    "./structs/return-expression/source.english.swa",
-			ExpectedLLIR: "./structs/return-expression/source.english.ll",
-			OutputPath:   "9d87488a-e878-4943-aaf0-7e8a4b09ea1f",
-			T:            t,
+			InputPath: "./structs/return-expression/source.english.swa",
+			T:         t,
 		}
 
 		defer req.Cleanup()
@@ -112,10 +102,8 @@ func TestStructPropertyInReturnExpression(t *testing.T) {
 
 	t.Run("French", func(t *testing.T) {
 		req := CompileRequest{
-			InputPath:    "./structs/return-expression/source.french.swa",
-			ExpectedLLIR: "./structs/return-expression/source.french.ll",
-			OutputPath:   "ca2cd604-76a7-40d8-992d-186adedbe12a",
-			T:            t,
+			InputPath: "./structs/return-expression/source.french.swa",
+			T:         t,
 		}
 
 		defer req.Cleanup()
