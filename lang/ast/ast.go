0cad4b5462951f861fb857b15f1fb75cc69bb6e9
pathe <pathe.sene@gmail.com>
2025-10-31 22:59:09 +0000

    Implement MarshalJSON

--- lang/ast/ast.go
+++ lang/ast/ast.go
@@ -3,9 +3,11 @@ package ast
 // Statement ...
 type Statement interface {
 	CompileLLVM(ctx *CompilerCtx) (error, *CompilerResult)
+	MarshalJSON() ([]byte, error)
 }
 
 // Expression ...
 type Expression interface {
 	CompileLLVM(ctx *CompilerCtx) (error, *CompilerResult)
+	MarshalJSON() ([]byte, error)
 }
--- lang/ast/call_expression.go
+++ lang/ast/call_expression.go
@@ -1,5 +1,7 @@
 package ast
 
+import "encoding/json"
+
 // CallExpression ...
 type CallExpression struct {
 	Arguments []Expression
@@ -10,4 +12,15 @@ var _ Expression = (*CallExpression)(nil)
 
 func (CallExpression) CompileLLVM(ctx *CompilerCtx) (error, *CompilerResult) {
 	panic("CallExpression compilation is not implemented")
+}
+
+func (expr CallExpression) MarshalJSON() ([]byte, error) {
+	m := make(map[string]any)
+	m["Caller"] = expr.Caller
+	m["Arguments"] = expr.Arguments
+
+	res := make(map[string]any)
+	res["ast.CallExpression"] = m
+
+	return json.Marshal(res)
 }
