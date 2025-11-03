8a8b62d70626eee611a5b86f314768d41e2f79a1
pathe <pathe.sene@gmail.com>
2025-11-03 09:34:41 +0000

    feat: add ast node for function calls

--- lang/parser/lookups.go
+++ lang/parser/lookups.go
@@ -86,4 +86,5 @@ func createTokenLookups() {
 	statement(lexer.Main, ParseMainStatement)
 	statement(lexer.Function, ParseFunctionDeclaration)
 	statement(lexer.Return, ParseReturnStatement)
+	led(lexer.OpenParen, Call, ParseFunctionCall)
 }
