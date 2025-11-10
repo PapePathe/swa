c91ff33cf20be5079c4ff047e57568e5443dd784
pathe <pathe.sene@gmail.com>
2025-11-10 12:19:42 +0000

    chore: add french version for while statement tests

--- tests/while_test.go
+++ tests/while_test.go
@@ -7,10 +7,10 @@ import (
 func TestWhileStatement(t *testing.T) {
 	t.Parallel()
 
-	t.Run("English", func(t *testing.T) {
+	t.Run("French", func(t *testing.T) {
 		t.Run("Iterate from ten to zero", func(t *testing.T) {
 			req := CompileRequest{
-				InputPath:               "./while/from-ten-to-zero.source.swa",
+				InputPath:               "./while/from-ten-to-zero.french.swa",
 				T:                       t,
 				ExpectedExecutionOutput: "10 9 8 7 6 5 4 3 2 1 ",
 			}
@@ -20,7 +20,29 @@ func TestWhileStatement(t *testing.T) {
 
 		t.Run("Iterate from zero to ten", func(t *testing.T) {
 			req := CompileRequest{
-				InputPath:               "./while/from-zero-to-ten.source.swa",
+				InputPath:               "./while/from-zero-to-ten.french.swa",
+				T:                       t,
+				ExpectedExecutionOutput: "0 1 2 3 4 5 6 7 8 9 ",
+			}
+
+			req.AssertCompileAndExecute()
+		})
+	})
+
+	t.Run("English", func(t *testing.T) {
+		t.Run("Iterate from ten to zero", func(t *testing.T) {
+			req := CompileRequest{
+				InputPath:               "./while/from-ten-to-zero.english.swa",
+				T:                       t,
+				ExpectedExecutionOutput: "10 9 8 7 6 5 4 3 2 1 ",
+			}
+
+			req.AssertCompileAndExecute()
+		})
+
+		t.Run("Iterate from zero to ten", func(t *testing.T) {
+			req := CompileRequest{
+				InputPath:               "./while/from-zero-to-ten.english.swa",
 				T:                       t,
 				ExpectedExecutionOutput: "0 1 2 3 4 5 6 7 8 9 ",
 			}
