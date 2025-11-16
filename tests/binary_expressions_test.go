b2a5a98cf13335eaffd30c8b2df28884d7872c33
pathe <pathe.sene@gmail.com>
2025-11-16 15:59:04 +0000

    feat: add integration tests for binary expression

---
+++ tests/binary_expressions_test.go
@@ -0,0 +1,30 @@
+package tests
+
+import (
+	"testing"
+)
+
+func TestBinaryExpressionsModulo(t *testing.T) {
+	t.Parallel()
+
+	t.Run("English", func(t *testing.T) {
+		t.Run("Modulo with symbol expressions", func(t *testing.T) {
+			req := CompileRequest{
+				InputPath:               "./binary-expressions/modulo/variable-declaration-2.english.swa",
+				T:                       t,
+				ExpectedExecutionOutput: "v1: 0, v2: 1",
+			}
+
+			req.AssertCompileAndExecute()
+		})
+		t.Run("Modulo with integer expressions", func(t *testing.T) {
+			req := CompileRequest{
+				InputPath:               "./binary-expressions/modulo/variable-declaration.english.swa",
+				T:                       t,
+				ExpectedExecutionOutput: "x: 1, y: 0",
+			}
+
+			req.AssertCompileAndExecute()
+		})
+	})
+}
