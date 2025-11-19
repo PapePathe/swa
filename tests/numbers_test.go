0affe8c03511678db85639f44ed27fba1676b2db
pathe <pathe.sene@gmail.com>
2025-11-19 20:21:06 +0000

    feat: add dedicated tests for numbers

--- tests/numbers_test.go
+++ tests/numbers_test.go
@@ -10,7 +10,17 @@ func TestSignedNumbers(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
 		req := CompileRequest{
 			InputPath:               "./numbers/signed.english.swa",
-			ExpectedExecutionOutput: "x+y: 0, x*y: -4",
+			ExpectedExecutionOutput: "x+y: 0, x*y: -4, x-y: -4, x/y: -1, x%y: 0",
+			T:                       t,
+		}
+
+		req.AssertCompileAndExecute()
+	})
+
+	t.Run("French", func(t *testing.T) {
+		req := CompileRequest{
+			InputPath:               "./numbers/signed.french.swa",
+			ExpectedExecutionOutput: "x+y: 0, x*y: -4, x-y: -4, x/y: -1, x%y: 0",
 			T:                       t,
 		}
 
