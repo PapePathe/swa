29bbfc97d8f7aac6baea9677f52594f5a3d4d8ff
pathe <pathe.sene@gmail.com>
2025-11-20 05:26:39 +0000

    Integration: add tests for arrays of floats

--- tests/numbers_test.go
+++ tests/numbers_test.go
@@ -8,13 +8,65 @@ func TestSignedNumbers(t *testing.T) {
 	t.Parallel()
 
 	t.Run("English", func(t *testing.T) {
-		req := CompileRequest{
-			InputPath:               "./numbers/signed.english.swa",
-			ExpectedExecutionOutput: "x+y: 0, x*y: -4, x-y: -4, x/y: -1, x%y: 0",
-			T:                       t,
-		}
+		t.Run("Signed", func(t *testing.T) {
+			req := CompileRequest{
+				InputPath:               "./numbers/signed.english.swa",
+				ExpectedExecutionOutput: "x+y: 0, x*y: -4, x-y: -4, x/y: -1, x%y: 0",
+				T:                       t,
+			}
 
-		req.AssertCompileAndExecute()
+			req.AssertCompileAndExecute()
+		})
+
+		t.Run("integer arithmetic", func(t *testing.T) {
+			req := CompileRequest{
+				InputPath:               "./numbers/integer-arithmetic/source.english.swa",
+				ExpectedExecutionOutput: "okokokokokok",
+				T:                       t,
+			}
+
+			req.AssertCompileAndExecute()
+		})
+
+		t.Run("float-arithmetic", func(t *testing.T) {
+			req := CompileRequest{
+				InputPath:               "./numbers/float-arithmetic/source.english.swa",
+				ExpectedExecutionOutput: "okokokokokok",
+				T:                       t,
+			}
+
+			req.AssertCompileAndExecute()
+		})
+
+		t.Run("integer-arrays", func(t *testing.T) {
+			req := CompileRequest{
+				InputPath:               "./numbers/integer-arrays/source.english.swa",
+				ExpectedExecutionOutput: "okokok",
+				T:                       t,
+			}
+
+			req.AssertCompileAndExecute()
+		})
+
+		t.Run("float-arrays", func(t *testing.T) {
+			req := CompileRequest{
+				InputPath:               "./numbers/float-arrays/source.english.swa",
+				ExpectedExecutionOutput: "okokok",
+				T:                       t,
+			}
+
+			req.AssertCompileAndExecute()
+		})
+
+		t.Run("integer-float-separation", func(t *testing.T) {
+			req := CompileRequest{
+				InputPath:               "./numbers/integer-float-separation/source.english.swa",
+				ExpectedExecutionOutput: "",
+				T:                       t,
+			}
+
+			req.AssertCompileAndExecute()
+		})
 	})
 
 	t.Run("French", func(t *testing.T) {
