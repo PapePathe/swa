2173f327bc0f395a2cbfb98400a4e207ebd0df6f
pathe <pathe.sene@gmail.com>
2025-11-20 01:55:57 +0000

    Integration: added missing tests for functions in english dialect

--- tests/functions_test.go
+++ tests/functions_test.go
@@ -7,6 +7,53 @@ import (
 func TestFunctions(t *testing.T) {
 	t.Parallel()
 
+	t.Run("English", func(t *testing.T) {
+		t.Run("Substract", func(t *testing.T) {
+			req := CompileRequest{
+				InputPath:               "./functions/substract.source.english.swa",
+				ExpectedOutput:          "",
+				ExpectedExecutionOutput: "10 - 5 = 5",
+				T:                       t,
+			}
+
+			req.AssertCompileAndExecute()
+		})
+
+		t.Run("Add", func(t *testing.T) {
+			req := CompileRequest{
+				InputPath:               "./functions/add.source.english.swa",
+				ExpectedOutput:          "",
+				ExpectedExecutionOutput: "10 + 5 = 15",
+				T:                       t,
+			}
+
+			req.AssertCompileAndExecute()
+		})
+
+		t.Run("Divide", func(t *testing.T) {
+			req := CompileRequest{
+				InputPath:               "./functions/divide.source.english.swa",
+				ExpectedOutput:          "",
+				ExpectedExecutionOutput: "10 / 5 = 2",
+				T:                       t,
+			}
+
+			req.AssertCompileAndExecute()
+		})
+
+		t.Run("Multiply", func(t *testing.T) {
+			req := CompileRequest{
+				InputPath:               "./functions/multiply.source.english.swa",
+				ExpectedOutput:          "",
+				ExpectedExecutionOutput: "10 * 5 = 50",
+				T:                       t,
+			}
+
+			req.AssertCompileAndExecute()
+		})
+
+	})
+
 	t.Run("French", func(t *testing.T) {
 		t.Run("Substract", func(t *testing.T) {
 			req := CompileRequest{
