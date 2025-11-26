12f3fef276bc16d0af3d8abbc0f209f0037430c0
pathe <pathe.sene@gmail.com>
2025-11-26 03:34:47 +0000

    Feat: support parsing external function declarations

--- lang/parser/function_declaration.go
+++ lang/parser/function_declaration.go
@@ -38,13 +38,17 @@ func ParseFunctionDeclaration(p *Parser) (ast.Statement, error) {
 	funDecl.ReturnType = returnType
 	funDecl.Tokens = append(funDecl.Tokens, tokens...)
 
-	body, err := ParseBlockStatement(p)
-	if err != nil {
-		return nil, err
+	if p.currentToken().Kind == lexer.OpenBracket {
+		body, err := ParseBlockStatement(p)
+		if err != nil {
+			return nil, err
+		}
+
+		funDecl.Tokens = append(funDecl.Tokens, body.TokenStream()...)
+		funDecl.Body = body
 	}
 
-	funDecl.Tokens = append(funDecl.Tokens, body.TokenStream()...)
-	funDecl.Body = body
+	funDecl.Tokens = append(funDecl.Tokens, p.expect(lexer.SemiColon))
 
 	return funDecl, nil
 }
