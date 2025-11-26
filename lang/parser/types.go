ef2e621f4d5c869206186320e20d6be9e79d6b8f
pathe <pathe.sene@gmail.com>
2025-11-26 03:34:08 +0000

    Feat: add int64 to types parser

--- lang/parser/types.go
+++ lang/parser/types.go
@@ -32,6 +32,7 @@ func typeNud(kind lexer.TokenKind, nudFn TypeNudHandlerFunc) {
 func createTokenTypeLookups() {
 	typeNud(lexer.Identifier, parseSymbolType)
 	typeNud(lexer.TypeInt, parseIntType)
+	typeNud(lexer.TypeInt64, parseInt64Type)
 	typeNud(lexer.TypeFloat, parseFloatType)
 	typeNud(lexer.TypeString, parseStringType)
 	typeNud(lexer.OpenBracket, parseArrayType)
@@ -47,6 +48,12 @@ func parseStringType(p *Parser) (ast.Type, []lexer.Token) {
 	tokens := []lexer.Token{p.advance()}
 
 	return ast.StringType{}, tokens
+}
+
+func parseInt64Type(p *Parser) (ast.Type, []lexer.Token) {
+	tokens := []lexer.Token{p.advance()}
+
+	return ast.Number64Type{}, tokens
 }
 
 func parseIntType(p *Parser) (ast.Type, []lexer.Token) {
