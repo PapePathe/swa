ac15c7628c01264bdb058b5ed312191ce6085cf0
pathe <pathe.sene@gmail.com>
2025-11-28 08:27:35 +0000

    feat: prep for lexer with dialect specified

--- lang/lexer/tokenize.go
+++ lang/lexer/tokenize.go
@@ -13,27 +13,7 @@ func Tokenize(source string) ([]Token, Dialect) {
 		os.Exit(1)
 	}
 
-	for !lex.atEOF() {
-		matched := false
-
-		for _, pattern := range lex.patterns {
-			loc := pattern.regex.FindStringIndex(lex.remainder())
-
-			if loc != nil && loc[0] == 0 {
-				pattern.handler(lex, pattern.regex)
-
-				matched = true
-
-				break
-			}
-		}
-
-		if !matched {
-			panic(fmt.Sprintf("Lexer::Error -> unrecognized token near %s", lex.remainder()))
-		}
-	}
-
-	lex.push(NewToken(EOF, "EOF", lex.line))
+	lex.tokenizeLoop()
 
 	return lex.Tokens, dialect
 }
