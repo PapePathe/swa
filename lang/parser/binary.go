592fd465743c97650be94282c9fcd4d2f79eb4ee
pathe <pathe.sene@gmail.com>
2025-11-28 18:23:19 +0000

    Feat: parse namespaces function calls

--- lang/parser/binary.go
+++ lang/parser/binary.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"swahili/lang/ast"
+	"swahili/lang/lexer"
 )
 
 // ParseBinaryExpression ...
@@ -21,6 +22,26 @@ func ParseBinaryExpression(p *Parser, left ast.Expression, bp BindingPower) (ast
 
 	expr.Right = right
 	expr.Tokens = append(expr.Tokens, right.TokenStream()...)
+
+	return expr, nil
+}
+
+// ParseBinaryExpression ...
+func ParsePackageAccessExpression(p *Parser, left ast.Expression, bp BindingPower) (ast.Expression, error) {
+	expr := ast.PackageAccessExpression{}
+	p.currentExpression = &expr
+
+	expr.Namespaces = append(expr.Namespaces, left)
+
+	for p.hasTokens(); p.currentToken().Kind == lexer.DoubleColon; {
+		p.expect(lexer.DoubleColon)
+		id := p.expect(lexer.Identifier)
+		expr.Namespaces = append(expr.Namespaces, ast.SymbolExpression{Value: id.Value})
+	}
+
+	if p.currentToken().Kind == lexer.OpenParen {
+		return ParseFunctionCall(p, expr, DefaultBindingPower)
+	}
 
 	return expr, nil
 }
