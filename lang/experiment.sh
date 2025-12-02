a26c49058442be904dedcb3d2705908f6f71ed54
pathe <pathe.sene@gmail.com>
2025-12-02 17:28:53 +0000

    Chore: enhance experiment script

--- lang/experiment.sh
+++ lang/experiment.sh
@@ -4,4 +4,13 @@ set -euxo pipefail
 
 go build -o tmp/swa main.go
 
-./tmp/swa compile -s test.swa && cat start.ll && ./start.exe
+if [ "$1" == "parse" ]; then
+    echo "🚀 Parsing..."
+    ./tmp/swa parse -s test.swa
+elif [ "$1" == "compile" ]; then
+    echo "🛑 Compiling..."
+    ./tmp/swa compile -s test.swa && cat start.ll && ./start.exe
+else
+    echo "❓ Unknown command: $1. Use 'compile' or 'parse'."
+    exit 1
+fi
