4859791b5c304237837c74ae06175717944fe4aa
pathe <pathe.sene@gmail.com>
2025-12-26 01:51:21 +0000

    wip: reduce interface bloat

--- lang/ast/visitor.go
+++ lang/ast/visitor.go
@@ -1,7 +1,6 @@
 package ast
 
-type CodeGenerator interface {
-	// Statements
+type StatementsCodeGenerator interface {
 	VisitBlockStatement(node *BlockStatement) error
 	VisitVarDeclaration(node *VarDeclarationStatement) error
 	VisitReturnStatement(node *ReturnStatement) error
@@ -12,8 +11,9 @@ type CodeGenerator interface {
 	VisitStructDeclaration(node *StructDeclarationStatement) error
 	VisitPrintStatement(node *PrintStatetement) error
 	VisitMainStatement(node *MainStatement) error
+}
 
-	// Expressions
+type ExpressionsCodeGenerator interface {
 	VisitBinaryExpression(node *BinaryExpression) error
 	VisitFunctionCall(node *FunctionCallExpression) error
 	VisitCallExpression(node *CallExpression) error
@@ -28,4 +28,11 @@ type CodeGenerator interface {
 	VisitArrayInitializationExpression(node *ArrayInitializationExpression) error
 	VisitArrayOfStructsAccessExpression(node *ArrayOfStructsAccessExpression) error
 	VisitPrefixExpression(node *PrefixExpression) error
+}
+
+type CodeGenerator interface {
+	// Statements
+	StatementsCodeGenerator
+	// Expressions
+	ExpressionsCodeGenerator
 }
