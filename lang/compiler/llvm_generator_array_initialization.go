5da9834efeb1f07b509de462d4e27ea4d1cd9f03
pathe <pathe.sene@gmail.com>
2025-12-27 01:25:17 +0000

    chore: extract to files

---
+++ lang/compiler/llvm_generator_array_initialization.go
@@ -0,0 +1,144 @@
+package compiler
+
+import (
+	"fmt"
+	"reflect"
+	"swahili/lang/ast"
+
+	"tinygo.org/x/go-llvm"
+)
+
+type ElementInjector func(
+	g *LLVMGenerator,
+	expr ast.Expression,
+	targetAddr llvm.Value,
+) (error, *ast.StructSymbolTableEntry)
+
+var ArrayInitializationExpressionInjectors = map[reflect.Type]ElementInjector{
+	reflect.TypeFor[ast.SymbolExpression]():               injectSymbol,
+	reflect.TypeFor[ast.NumberExpression]():               injectLiteral,
+	reflect.TypeFor[ast.FloatExpression]():                injectLiteral,
+	reflect.TypeFor[ast.StringExpression]():               injectLiteral,
+	reflect.TypeFor[ast.StructInitializationExpression](): injectStruct,
+}
+
+func (g *LLVMGenerator) VisitArrayInitializationExpression(node *ast.ArrayInitializationExpression) error {
+	err, llvmtyp := node.Underlying.LLVMType(g.Ctx)
+	if err != nil {
+		return err
+	}
+
+	var discoveredEntry *ast.StructSymbolTableEntry
+
+	arrayPointer := g.Ctx.Builder.CreateAlloca(llvmtyp, "array_alloc")
+
+	for i, expr := range node.Contents {
+		itemGep := g.Ctx.Builder.CreateGEP(llvmtyp, arrayPointer, []llvm.Value{
+			llvm.ConstInt(llvm.GlobalContext().Int32Type(), 0, false),
+			llvm.ConstInt(llvm.GlobalContext().Int32Type(), uint64(i), false),
+		}, "")
+
+		injector, ok := ArrayInitializationExpressionInjectors[reflect.TypeOf(expr)]
+		if !ok {
+			return fmt.Errorf("unsupported array initialization element: %T", expr)
+		}
+
+		err, sEntry := injector(g, expr, itemGep)
+		if err != nil {
+			return err
+		}
+
+		if discoveredEntry == nil && sEntry != nil {
+			discoveredEntry = sEntry
+		}
+	}
+
+	g.setLastResult(&ast.CompilerResult{
+		Value: &arrayPointer,
+		ArraySymbolTableEntry: &ast.ArraySymbolTableEntry{
+			ElementsCount:     llvmtyp.ArrayLength(),
+			UnderlyingTypeDef: discoveredEntry, // Correctly passed up!
+			UnderlyingType:    llvmtyp.ElementType(),
+			Type:              llvmtyp,
+		},
+	})
+
+	return nil
+}
+
+func injectLiteral(g *LLVMGenerator, expr ast.Expression, targetAddr llvm.Value) (error, *ast.StructSymbolTableEntry) {
+	err := expr.Accept(g)
+	if err != nil {
+		return err, nil
+	}
+
+	res := g.getLastResult()
+	g.Ctx.Builder.CreateStore(*res.Value, targetAddr)
+
+	return nil, nil // Literals don't define a struct subtype
+}
+
+func injectSymbol(g *LLVMGenerator, expr ast.Expression, targetAddr llvm.Value) (error, *ast.StructSymbolTableEntry) {
+	err := expr.Accept(g)
+	if err != nil {
+		return err, nil
+	}
+
+	res := g.getLastResult()
+
+	// If it's a struct/complex type, we use the Ref (SymbolTableEntry) to find the type
+	var loadType llvm.Type
+
+	var structEntry *ast.StructSymbolTableEntry
+
+	if res.SymbolTableEntry != nil && res.SymbolTableEntry.Ref != nil {
+		loadType = res.SymbolTableEntry.Ref.LLVMType
+		structEntry = res.SymbolTableEntry.Ref
+	} else {
+		loadType = res.Value.Type()
+	}
+
+	val := g.Ctx.Builder.CreateLoad(loadType, *res.Value, "arr.load.sym")
+	g.Ctx.Builder.CreateStore(val, targetAddr)
+
+	return nil, structEntry
+}
+
+func injectStruct(g *LLVMGenerator, expr ast.Expression, targetAddr llvm.Value) (error, *ast.StructSymbolTableEntry) {
+	node, _ := expr.(ast.StructInitializationExpression)
+
+	err, tblEntry := g.Ctx.FindStructSymbol(node.Name)
+	if err != nil {
+		return err, nil
+	}
+
+	for _, fieldName := range node.Properties {
+		err, idx := tblEntry.Metadata.PropertyIndex(fieldName)
+		if err != nil {
+			return err, nil
+		}
+
+		fieldNode := node.Values[idx]
+
+		err = fieldNode.Accept(g)
+		if err != nil {
+			return err, nil
+		}
+
+		fieldRes := g.getLastResult()
+
+		fieldGep := g.Ctx.Builder.CreateGEP(
+			tblEntry.LLVMType,
+			targetAddr,
+			[]llvm.Value{
+				llvm.ConstInt(llvm.GlobalContext().Int32Type(), 0, false),
+				llvm.ConstInt(llvm.GlobalContext().Int32Type(), uint64(idx), false),
+			},
+			fmt.Sprintf("field.%s", fieldName),
+		)
+
+		g.Ctx.Builder.CreateStore(*fieldRes.Value, fieldGep)
+	}
+
+	return nil, tblEntry
+}
---
+++ lang/compiler/llvm_generator_struct_initialization.go
@@ -0,0 +1,104 @@
+package compiler
+
+import (
+	"fmt"
+	"reflect"
+	"swahili/lang/ast"
+
+	"tinygo.org/x/go-llvm"
+)
+
+func (g *LLVMGenerator) VisitStructInitializationExpression(node *ast.StructInitializationExpression) error {
+	err, structType := g.Ctx.FindStructSymbol(node.Name)
+	if err != nil {
+		return err
+	}
+
+	instance := g.Ctx.Builder.CreateAlloca(structType.LLVMType, node.Name+".instance")
+
+	for _, name := range node.Properties {
+		err, propIndex := structType.Metadata.PropertyIndex(name)
+		if err != nil {
+			return err
+		}
+
+		expr := node.Values[propIndex]
+
+		err = expr.Accept(g)
+		if err != nil {
+			return err
+		}
+
+		res := g.getLastResult()
+		fieldAddr := g.Ctx.Builder.CreateStructGEP(
+			structType.LLVMType,
+			instance,
+			propIndex,
+			fmt.Sprintf("%s.%s", node.Name, name),
+		)
+
+		injector, ok := structInjectors[reflect.TypeOf(expr)]
+		if !ok {
+			return fmt.Errorf("struct field initialization unimplemented for %T", expr)
+		}
+
+		targetType := structType.PropertyTypes[propIndex]
+		injector(g, res, fieldAddr, targetType)
+	}
+
+	g.setLastResult(&ast.CompilerResult{
+		Value:                  &instance,
+		StructSymbolTableEntry: structType,
+	})
+
+	return nil
+}
+
+type StructInitializationExpressionPropertyInjector func(
+	g *LLVMGenerator,
+	res *ast.CompilerResult,
+	fieldAddr llvm.Value,
+	targetType llvm.Type,
+)
+
+var structInjectors = map[reflect.Type]StructInitializationExpressionPropertyInjector{
+	reflect.TypeFor[ast.NumberExpression]():              injectDirectly,
+	reflect.TypeFor[ast.FloatExpression]():               injectDirectly,
+	reflect.TypeFor[ast.StringExpression]():              injectDirectly,
+	reflect.TypeFor[ast.SymbolExpression]():              injectWithArrayDecay,
+	reflect.TypeFor[ast.BinaryExpression]():              injectWithArrayDecay,
+	reflect.TypeFor[ast.ArrayInitializationExpression](): injectArrayLiteral,
+	reflect.TypeFor[ast.ArrayAccessExpression]():         injectArrayAccess,
+}
+
+func injectDirectly(g *LLVMGenerator, res *ast.CompilerResult, fieldAddr llvm.Value, targetType llvm.Type) {
+	g.Ctx.Builder.CreateStore(*res.Value, fieldAddr)
+}
+
+func injectWithArrayDecay(g *LLVMGenerator, res *ast.CompilerResult, fieldAddr llvm.Value, targetType llvm.Type) {
+	_, isArray := res.SymbolTableEntry.DeclaredType.(ast.ArrayType)
+
+	if isArray && targetType.TypeKind() == llvm.PointerTypeKind {
+		ptr := g.Ctx.Builder.CreateBitCast(*res.SymbolTableEntry.Address, targetType, "decay")
+		g.Ctx.Builder.CreateStore(ptr, fieldAddr)
+
+		return
+	}
+
+	g.Ctx.Builder.CreateStore(*res.Value, fieldAddr)
+}
+
+func injectArrayLiteral(g *LLVMGenerator, res *ast.CompilerResult, fieldAddr llvm.Value, targetType llvm.Type) {
+	if targetType.TypeKind() == llvm.PointerTypeKind {
+		ptr := g.Ctx.Builder.CreateBitCast(*res.Value, targetType, "array.ptr.cast")
+		g.Ctx.Builder.CreateStore(ptr, fieldAddr)
+	} else {
+		load := g.Ctx.Builder.CreateLoad(res.ArraySymbolTableEntry.Type, *res.Value, "array.load")
+		g.Ctx.Builder.CreateStore(load, fieldAddr)
+	}
+}
+
+func injectArrayAccess(g *LLVMGenerator, res *ast.CompilerResult, fieldAddr llvm.Value, targetType llvm.Type) {
+	load := g.Ctx.Builder.CreateLoad(res.ArraySymbolTableEntry.UnderlyingType, *res.Value, "access.load")
+	g.Ctx.Builder.CreateStore(load, fieldAddr)
+}
