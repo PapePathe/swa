0427c7f1381d176c4756e543853d029e2d46c1b1
pathe <pathe.sene@gmail.com>
2026-01-09 11:01:52 +0000

    chore: add todos

--- lang/lexer/tokenize.go
+++ lang/lexer/tokenize.go
@@ -10,12 +10,14 @@ func Tokenize(source string) ([]Token, Dialect) {
 	lex, dialect, err := NewFastLexer(source)
 	if err != nil {
 		fmt.Println(err)
+		// TODO this method should return error instead of exiting the program
 		os.Exit(1)
 	}
 
 	tokens, err := lex.GetAllTokens()
 	if err != nil {
 		fmt.Println(err)
+		// TODO this method should return error instead of exiting the program
 		os.Exit(1)
 	}
 
