ddbd93e1ca24ab4da8f03ef4a690cd592271df12
pathe <pathe.sene@gmail.com>
2026-01-31 04:57:56 +0000

    feat: support simple condition in return statement

--- lang/parser/return_statement.go
+++ lang/parser/return_statement.go
@@ -17,6 +17,26 @@ func ParseReturnStatement(p *Parser) (ast.Statement, error) {
 
 	stmt.Value = value
 	stmt.Tokens = append(stmt.Tokens, value.TokenStream()...)
+
+	if p.currentToken().Kind == lexer.KeywordIf {
+		cond := ast.ConditionalStatetement{}
+		p.expect(lexer.KeywordIf)
+		p.expect(lexer.OpenParen)
+		value, err := parseExpression(p, DefaultBindingPower)
+		if err != nil {
+			return nil, err
+		}
+
+		cond.Condition = value
+		cond.Success = ast.BlockStatement{
+			Body: []ast.Statement{stmt},
+		}
+		p.expect(lexer.CloseParen)
+		p.expect(lexer.SemiColon)
+
+		return cond, nil
+	}
+
 	stmt.Tokens = append(stmt.Tokens, p.expect(lexer.SemiColon))
 
 	return stmt, nil
