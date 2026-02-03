8068b566d78efaa137cb0611f6f9ed9bd6fe3517
pathe <pathe.sene@gmail.com>
2026-02-03 04:56:56 +0000

    chore: add zero expression

--- lang/ast/error_expression.go
+++ lang/ast/error_expression.go
@@ -5,6 +5,7 @@ import (
 )
 
 type ErrorExpression struct {
+	Exp    Expression
 	Tokens []lexer.Token
 }
 
--- lang/ast/expression_statement.go
+++ lang/ast/expression_statement.go
@@ -20,3 +20,25 @@ func (expr *ExpressionStatement) Accept(g CodeGenerator) error {
 func (expr ExpressionStatement) TokenStream() []lexer.Token {
 	return expr.Tokens
 }
+
+type ZeroExpression struct {
+	T      Type
+	Tokens []lexer.Token
+}
+
+// Accept implements [Expression].
+func (z *ZeroExpression) Accept(g CodeGenerator) error {
+	return g.VisitZeroExpression(z)
+}
+
+// TokenStream implements [Expression].
+func (z *ZeroExpression) TokenStream() []lexer.Token {
+	return z.Tokens
+}
+
+// VisitedSwaType implements [Expression].
+func (z *ZeroExpression) VisitedSwaType() Type {
+	panic("unimplemented")
+}
+
+var _ Expression = (*ZeroExpression)(nil)
