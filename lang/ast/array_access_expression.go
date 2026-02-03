547439dfa53b525a828462750028a028e61ae00b
pathe <pathe.sene@gmail.com>
2026-02-03 03:17:40 +0000

    feat: implement stringer for array expr

--- lang/ast/array_access_expression.go
+++ lang/ast/array_access_expression.go
@@ -1,6 +1,7 @@
 package ast
 
 import (
+	"fmt"
 	"swahili/lang/lexer"
 )
 
@@ -17,6 +18,9 @@ func (expr *ArrayAccessExpression) Accept(g CodeGenerator) error {
 	return g.VisitArrayAccessExpression(expr)
 }
 
+func (expr ArrayAccessExpression) String() string {
+	return fmt.Sprintf("%s[%s]", expr.Name, expr.Index)
+}
 func (expr ArrayAccessExpression) TokenStream() []lexer.Token {
 	return expr.Tokens
 }
