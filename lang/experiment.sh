9210b18ce6527dd057991a18cc47dbe58f0a64c9
pathe <pathe.sene@gmail.com>
2026-02-03 03:24:26 +0000

    chore: cleanup experiment stuff

--- lang/experiment.sh
+++ lang/experiment.sh
@@ -6,14 +6,14 @@ go build -o tmp/swa main.go
 
 if [ "$1" == "parse" ]; then
     ./tmp/swa parse -s test.swa
+elif [ "$1" == "parse-tree" ]; then
+    ./tmp/swa parse -s test.swa -o tree
 elif [ "$1" == "tokenize" ]; then
     ./tmp/swa tokenize -s test.swa
 elif [ "$1" == "compile" ]; then
     ./tmp/swa compile -s test.swa && ./start.exe
-elif [ "$1" == "compile-exp-debug" ]; then
+elif [ "$1" == "compile-trace" ]; then
     SWA_DEBUG=yes ./tmp/swa compile -e -s test.swa && ./start.exe
-elif [ "$1" == "compile-exp" ]; then
-    ./tmp/swa compile -e -s test.swa && ./start.exe
 else
     echo "❓ Unknown command: $1. Use 'compile' or 'parse'."
     exit 1
