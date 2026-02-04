88fb1fbdf1e5e8e5f732edaf57566ec611a97928
pathe <pathe.sene@gmail.com>
2026-02-04 16:33:11 +0000

    chore: extract zero expr to a file

--- lang/ast/expression_statement.go
+++ lang/ast/expression_statement.go
@@ -4,9 +4,7 @@ import (
 	"swahili/lang/lexer"
 )
 
-// ExpressionStatement ...
 type ExpressionStatement struct {
-	// The expression
 	Exp    Expression
 	Tokens []lexer.Token
 }
@@ -20,25 +18,3 @@ func (expr *ExpressionStatement) Accept(g CodeGenerator) error {
 func (expr ExpressionStatement) TokenStream() []lexer.Token {
 	return expr.Tokens
 }
-
-type ZeroExpression struct {
-	T      Type
-	Tokens []lexer.Token
-}
-
-// Accept implements [Expression].
-func (z *ZeroExpression) Accept(g CodeGenerator) error {
-	return g.VisitZeroExpression(z)
-}
-
-// TokenStream implements [Expression].
-func (z *ZeroExpression) TokenStream() []lexer.Token {
-	return z.Tokens
-}
-
-// VisitedSwaType implements [Expression].
-func (z *ZeroExpression) VisitedSwaType() Type {
-	panic("unimplemented")
-}
-
-var _ Expression = (*ZeroExpression)(nil)
