41ba238dd2c7240eff01a9bd718ab0b401ea9a69
pathe <pathe.sene@gmail.com>
2026-02-06 17:58:04 +0000

    feat: support array of structs access expression

--- lang/compiler/llvm_generator_tuple.go
+++ lang/compiler/llvm_generator_tuple.go
@@ -66,3 +66,69 @@ func (g *LLVMGenerator) ZeroOfTupleType(node *ast.TupleType) error {
 	// TODO this should not be nil
 	return nil
 }
+
+func (g *LLVMGenerator) VisitTupleAssignmentExpression(node *ast.TupleAssignmentExpression) error {
+	old := g.logger.Step("VisitTupleAssignmentExpression")
+	defer g.logger.Restore(old)
+
+	// 1. Evaluate RHS
+	err := node.Value.Accept(g)
+	if err != nil {
+		return err
+	}
+
+	typ, ok := node.Value.VisitedSwaType().(*ast.TupleType)
+	if ok {
+		l1 := len(typ.Types)
+		l2 := len(node.Assignees.Expressions)
+
+		if l1 != l2 {
+			return fmt.Errorf("length of tuples does not match expected %d got %d", l1, l2)
+		}
+	}
+
+	rhsRes := g.getLastResult()
+	if rhsRes == nil || rhsRes.Value == nil {
+		return fmt.Errorf("RHS of tuple assignment evaluated to nil")
+	}
+
+	rhsVal := *rhsRes.Value
+
+	// 2. Iterate Assignees
+	for i, expr := range node.Assignees.Expressions {
+		// Extract value at index i
+		elemVal := g.Ctx.Builder.CreateExtractValue(rhsVal, i, "")
+
+		// Store into assignee
+		switch a := expr.(type) {
+		case *ast.MemberExpression, *ast.ArrayAccessExpression,
+			*ast.ArrayOfStructsAccessExpression:
+			err := expr.Accept(g)
+			if err != nil {
+				return err
+			}
+
+			res := g.getLastResult()
+			g.Ctx.Builder.CreateStore(elemVal, *res.Value)
+		case *ast.SymbolExpression:
+			err, entry := g.Ctx.FindSymbol(a.Value)
+			if err != nil {
+				return err
+			}
+
+			if entry.Address == nil {
+				format := "cannot assign to symbol %s which has no address"
+
+				return fmt.Errorf(format, a.Value)
+			}
+
+			a.SwaType = entry.DeclaredType
+
+			g.Ctx.Builder.CreateStore(elemVal, *entry.Address)
+		default:
+			return fmt.Errorf("Unsupported assignee type in tuple assignment: %T", a)
+		}
+	}
+
+	return nil
+}
