ca7bcad3a8c7d73e0b1ea51e5fb9c2324c83d432
pathe <pathe.sene@gmail.com>
2026-02-06 17:42:23 +0000

    chore: add typechecker tests for tuple assignment

--- tests/tuples_test.go
+++ tests/tuples_test.go
@@ -2,6 +2,8 @@ package tests
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTupleReturns(t *testing.T) {
@@ -45,6 +47,17 @@ func TestTupleReturns(t *testing.T) {
 		req.AssertCompileAndExecute()
 	})
 
+	t.Run("Swap typecheck", func(t *testing.T) {
+		t.Parallel()
+		req := CompileRequest{
+			InputPath:      "./tuples/swap-typecheck.swa",
+			ExpectedOutput: "Expected assignment of Number but got Tuple\n",
+			T:              t,
+		}
+
+		assert.Error(t, req.Compile())
+	})
+
 	t.Run("Mixed Types", func(t *testing.T) {
 		t.Parallel()
 		req := CompileRequest{
@@ -73,5 +86,27 @@ func TestTupleReturns(t *testing.T) {
 			T:                       t,
 		}
 		req.AssertCompileAndExecute()
+	})
+
+	t.Run("Assign to member typecheck", func(t *testing.T) {
+		t.Parallel()
+		req := CompileRequest{
+			InputPath:      "./tuples/assign-to-member-expression-typecheck.swa",
+			ExpectedOutput: "Values mismatch in tuple expected Number got Error at index 0\n",
+			T:              t,
+		}
+
+		assert.Error(t, req.Compile())
+	})
+
+	t.Run("Assign to member typecheck length", func(t *testing.T) {
+		t.Parallel()
+		req := CompileRequest{
+			InputPath:      "./tuples/assign-to-member-expression-typecheck-length.swa",
+			ExpectedOutput: "length of tuples does not match expected 3 got 4\n",
+			T:              t,
+		}
+
+		assert.Error(t, req.Compile())
 	})
 }
