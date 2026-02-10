c94f2850949f7385fe6fdc06201277215d5aad6e
pathe <pathe.sene@gmail.com>
2026-02-10 12:02:04 +0000

    chore: harden multi dialect tests

--- tests/parser_test.go
+++ tests/parser_test.go
@@ -7,32 +7,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseNiceNumbers(t *testing.T) {
-	t.Run("French", func(t *testing.T) {
-		t.Run("json", func(t *testing.T) {
-			expected, _ := os.ReadFile("./parser/2.french.json")
-			req := CompileRequest{
-				InputPath:      "./parser/2.french.swa",
-				ExpectedOutput: string(expected),
-				T:              t,
-			}
-
-			assert.NoError(t, req.Parse("json"))
-		})
-
-		t.Run("tree", func(t *testing.T) {
-			expected, _ := os.ReadFile("./parser/2.french.tree")
-			req := CompileRequest{
-				InputPath:      "./parser/2.french.swa",
-				ExpectedOutput: string(expected),
-				T:              t,
-			}
-
-			assert.NoError(t, req.Parse("tree"))
-		})
-	})
-}
-
 func TestParser(t *testing.T) {
 
 	t.Run("English", func(t *testing.T) {
