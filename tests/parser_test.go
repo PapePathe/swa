c00af8e4dde1ba39f38d88a09f3abb9194f1d87c
pathe <pathe.sene@gmail.com>
2026-02-12 12:07:23 +0000

    chore: add scoping

--- tests/parser_test.go
+++ tests/parser_test.go
@@ -34,15 +34,28 @@ func TestParseNiceNumbers(t *testing.T) {
 }
 
 func TestParser(t *testing.T) {
-	t.Run("json", func(t *testing.T) {
-		expected, _ := os.ReadFile("./parser/1.english.json")
-		req := CompileRequest{
-			InputPath:      "./parser/1.english.swa",
-			ExpectedOutput: string(expected),
-			T:              t,
-		}
+	t.Run("English", func(t *testing.T) {
+		t.Run("json", func(t *testing.T) {
+			expected, _ := os.ReadFile("./parser/1.english.json")
+			req := CompileRequest{
+				InputPath:      "./parser/1.english.swa",
+				ExpectedOutput: string(expected),
+				T:              t,
+			}
 
-		assert.NoError(t, req.Parse("json"))
+			assert.NoError(t, req.Parse("json"))
+		})
+
+		t.Run("tree", func(t *testing.T) {
+			expected, _ := os.ReadFile("./parser/1.english.tree")
+			req := CompileRequest{
+				InputPath:      "./parser/1.english.swa",
+				ExpectedOutput: string(expected),
+				T:              t,
+			}
+
+			assert.NoError(t, req.Parse("tree"))
+		})
 	})
 
 	t.Run("tree", func(t *testing.T) {
