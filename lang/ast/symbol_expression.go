af7400323242b201453464a4492e466cacced322
pathe <pathe.sene@gmail.com>
2026-02-12 10:45:13 +0000

    feat: add ast nodes for symbol value and address

--- lang/ast/symbol_expression.go
+++ lang/ast/symbol_expression.go
@@ -28,3 +28,43 @@ func (expr SymbolExpression) TokenStream() []lexer.Token {
 func (expr SymbolExpression) VisitedSwaType() Type {
 	return expr.SwaType
 }
+
+type SymbolAdressExpression struct {
+	Exp     Expression
+	Tokens  []lexer.Token
+	SwaType Type
+}
+
+var _ Expression = (*SymbolAdressExpression)(nil)
+
+func (expr *SymbolAdressExpression) Accept(g CodeGenerator) error {
+	return g.VisitSymbolAdressExpression(expr)
+}
+
+func (expr SymbolAdressExpression) TokenStream() []lexer.Token {
+	return expr.Tokens
+}
+
+func (expr SymbolAdressExpression) VisitedSwaType() Type {
+	return expr.SwaType
+}
+
+type SymbolValueExpression struct {
+	Exp     Expression
+	Tokens  []lexer.Token
+	SwaType Type
+}
+
+var _ Expression = (*SymbolValueExpression)(nil)
+
+func (s *SymbolValueExpression) Accept(g CodeGenerator) error {
+	return g.VisitSymbolValueExpression(s)
+}
+
+func (s *SymbolValueExpression) TokenStream() []lexer.Token {
+	return s.Tokens
+}
+
+func (s *SymbolValueExpression) VisitedSwaType() Type {
+	return s.SwaType
+}
