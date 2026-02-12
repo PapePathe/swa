c3fc8b5aff3d37ef0c3e1b092163de7f9e298d79
pathe <pathe.sene@gmail.com>
2026-02-12 11:15:10 +0000

    chore: extract tracer to module

---
+++ lang/trc/trace.go
@@ -0,0 +1,40 @@
+package trc
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+type Logger struct {
+	dbg *log.Logger
+}
+
+func NewLogger(prefix string) *Logger {
+	l := Logger{
+		dbg: log.New(
+			os.Stdout,
+			fmt.Sprintf("%s ", prefix),
+			log.Lmsgprefix,
+		),
+	}
+
+	return &l
+}
+
+func (l Logger) Debug(msg string) {
+	l.dbg.Println(msg)
+}
+
+func (l Logger) Restore(prefix string) {
+	l.dbg.SetPrefix(prefix)
+}
+
+func (l Logger) Step(prefix string) string {
+	oldprefix := l.dbg.Prefix()
+	newprefix := fmt.Sprintf("%s %s ", oldprefix, prefix)
+
+	l.dbg.SetPrefix(newprefix)
+
+	return oldprefix
+}
