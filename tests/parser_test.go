64a214b8bc627a486da2808b892722361cc77696
pathe <pathe.sene@gmail.com>
2026-02-12 12:05:16 +0000

    integration: parse nice numbers

--- tests/parser_test.go
+++ tests/parser_test.go
@@ -7,6 +7,32 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestParseNiceNumbers(t *testing.T) {
+	t.Run("French", func(t *testing.T) {
+		t.Run("json", func(t *testing.T) {
+			expected, _ := os.ReadFile("./parser/2.french.json")
+			req := CompileRequest{
+				InputPath:      "./parser/2.french.swa",
+				ExpectedOutput: string(expected),
+				T:              t,
+			}
+
+			assert.NoError(t, req.Parse("json"))
+		})
+
+		t.Run("tree", func(t *testing.T) {
+			expected, _ := os.ReadFile("./parser/2.french.tree")
+			req := CompileRequest{
+				InputPath:      "./parser/2.french.swa",
+				ExpectedOutput: string(expected),
+				T:              t,
+			}
+
+			assert.NoError(t, req.Parse("tree"))
+		})
+	})
+}
+
 func TestParser(t *testing.T) {
 	t.Run("json", func(t *testing.T) {
 		expected, _ := os.ReadFile("./parser/1.english.json")
