d1c86e7d0442964b98e17f48dcdf4fc1322f7e64
pathe <pathe.sene@gmail.com>
2026-02-13 13:43:19 +0000

    integration: add more test cases

--- tests/regression_test.go
+++ tests/regression_test.go
@@ -7,6 +7,86 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestCollatz(t *testing.T) {
+	req := CompileRequest{
+		InputPath:               "./regression/collatz.swa",
+		ExpectedExecutionOutput: "Testing Collatz Conjecture...\nCollatz(27) took 111 steps.\nSuccess: Collatz(27) is correct.\n",
+		T:                       t,
+	}
+
+	req.AssertCompileAndExecute()
+}
+
+func TestBrainFuck(t *testing.T) {
+	req := CompileRequest{
+		InputPath:               "./regression/brainfuck.swa",
+		ExpectedExecutionOutput: "Testing Brainfuck Simulation...\nSimulating BF: +++[->+<]\nResult in cell 1: 3\nSuccess: BF simulation worked.\n",
+		T:                       t,
+	}
+
+	req.AssertCompileAndExecute()
+}
+
+func TestAckerman(t *testing.T) {
+	req := CompileRequest{
+		InputPath:               "./regression/ackermann.swa",
+		ExpectedExecutionOutput: "Testing Ackermann function...\nackermann(3, 2) = 29\nSuccess: Ackermann A(3, 2) is correct.\n",
+		T:                       t,
+	}
+
+	req.AssertCompileAndExecute()
+}
+
+func TestStringEdgeCases(t *testing.T) {
+	req := CompileRequest{
+		InputPath:               "./regression/string_edge_cases.swa",
+		ExpectedExecutionOutput: "s1: Hello\nWorld\ns2: Tab\tCharacter\ns3: Quote \" inside\ns4: Backslash \\ inside\nLong string: This is a very long string designed to test how the compiler handles larger literals in the assembly generation phase. It should be able to handle strings that are significantly longer than short identifiers or simple messages.\n",
+		T:                       t,
+	}
+
+	req.AssertCompileAndExecute()
+}
+
+func TestMultiDimensionalArrayAccess(t *testing.T) {
+	req := CompileRequest{
+		InputPath:      "./regression/multidim_array.swa",
+		ExpectedOutput: "TODO: nested array access not yet supported\n",
+		T:              t,
+	}
+
+	assert.Error(t, req.Compile())
+}
+
+func TestLargeBoolean(t *testing.T) {
+	req := CompileRequest{
+		InputPath:               "./regression/large_boolean.swa",
+		ExpectedExecutionOutput: "Success: Complex boolean logic evaluated correctly.\n",
+		T:                       t,
+	}
+
+	req.AssertCompileAndExecute()
+}
+
+func TestSymbolValue(t *testing.T) {
+	req := CompileRequest{
+		InputPath:      "./regression/symbol-value.swa",
+		ExpectedOutput: "TODO implement VisitSymbolValueExpression\n",
+		T:              t,
+	}
+
+	assert.Error(t, req.Compile())
+}
+
+func TestSymbolAddress(t *testing.T) {
+	req := CompileRequest{
+		InputPath:      "./regression/symbol-address.swa",
+		ExpectedOutput: "TODO implement VisitSymbolAdressExpression\n",
+		T:              t,
+	}
+
+	assert.Error(t, req.Compile())
+}
+
 func TestDoublePointer(t *testing.T) {
 	t.Run("french", func(t *testing.T) {
 		t.Run("compile", func(t *testing.T) {
