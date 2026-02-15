36eb8fd99aeebf0440d6900eb66a559be0415908
pathe <pathe.sene@gmail.com>
2026-02-15 18:40:59 +0000

    chore: cleanup tests

--- tests/struct_test.go
+++ tests/struct_test.go
@@ -2,8 +2,6 @@ package tests
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestEmptyStruct(t *testing.T) {
@@ -25,13 +23,9 @@ func TestEmptyStruct(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			req := CompileRequest{
-				InputPath:      test.inputPath,
-				ExpectedOutput: test.expectedOutput,
-				T:              t,
-			}
-
-			assert.Error(t, req.Compile())
+			NewFailedCompileRequest(t,
+				test.inputPath,
+				test.expectedOutput)
 		})
 	}
 
@@ -41,72 +35,40 @@ func TestStructZeroValues(t *testing.T) {
 	expected := "- Dimension with zero value\nLength: 0.00, Width: 0.00, Height: 0, Name: \n\n- Object with zero value\nLength: 0.00, Width: 0.00, Height: 0, Name: , Shape: \n\n- Block with zero value\nTop(Length: 0.00, Width: 0.00, Height: 0, Name: , Shape: )\nBottom(Length: 0.00, Width: 0.00, Height: 0, Name: , Shape: )\n"
 
 	t.Run("Structs with explicit zero initializer", func(t *testing.T) {
-		t.Parallel()
-
-		req := CompileRequest{
-			InputPath:               "./structs/zero-values.swa",
-			ExpectedExecutionOutput: expected,
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/zero-values.swa",
+			expected)
 	})
 
 	t.Run("Structs with implicit zero initializer", func(t *testing.T) {
-		t.Parallel()
-
-		req := CompileRequest{
-			InputPath:               "./structs/zero-values-implicit.swa",
-			ExpectedExecutionOutput: expected,
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/zero-values-implicit.swa",
+			expected)
 	})
 
 	t.Run("Array of structs", func(t *testing.T) {
-		t.Parallel()
-
-		req := CompileRequest{
-			InputPath:               "./structs/zero-values-array.swa",
-			ExpectedExecutionOutput: "- [1]Path with zero value\nx1: 0 y1:0",
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/zero-values-array.swa",
+			"- [1]Path with zero value\nx1: 0 y1:0")
 	})
 }
 
 func TestStructWithUnknownType(t *testing.T) {
 	t.Run("French", func(t *testing.T) {
-		t.Parallel()
-
-		req := CompileRequest{
-			InputPath:      "./structs/unknown-property-type/source.french.swa",
-			ExpectedOutput: "struct named unknown_type does not exist in symbol table\n",
-			T:              t,
-		}
-
-		assert.Error(t, req.Compile())
+		NewFailedCompileRequest(t,
+			"./structs/unknown-property-type/source.french.swa",
+			"struct named unknown_type does not exist in symbol table\n")
 	})
 
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-
-		req := CompileRequest{
-			InputPath:      "./structs/unknown-property-type/source.english.swa",
-			ExpectedOutput: "struct named unknown_type does not exist in symbol table\n",
-			T:              t,
-		}
-
-		assert.Error(t, req.Compile())
+		NewFailedCompileRequest(t,
+			"./structs/unknown-property-type/source.english.swa",
+			"struct named unknown_type does not exist in symbol table\n")
 	})
 }
 
 func TestStructPropertyAssignment(t *testing.T) {
 	t.Run("French", func(t *testing.T) {
-		t.Parallel()
-
 		req := CompileRequest{
 			InputPath: "./structs/assignment/source.french.swa",
 			T:         t,
@@ -116,8 +78,6 @@ func TestStructPropertyAssignment(t *testing.T) {
 	})
 
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-
 		req := CompileRequest{
 			InputPath: "./structs/assignment/source.english.swa",
 			T:         t,
@@ -129,202 +89,119 @@ func TestStructPropertyAssignment(t *testing.T) {
 
 func TestStructAll(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-
 		expected := "Name: (Pathe), TechStack: (Ruby, Rust, Go), Age: (40), Height: (1.80)"
-		req := CompileRequest{
-			InputPath:               "./structs/all/source.english.swa",
-			ExpectedExecutionOutput: expected,
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/all/source.english.swa",
+			expected)
 	})
 
 	t.Run("French", func(t *testing.T) {
-		t.Parallel()
-
 		expected := "Nom: (Pathe), Stack Technique: (Ruby, Rust, Go), Age: (40), Taille: (1.80)"
-		req := CompileRequest{
-			InputPath:               "./structs/all/source.french.swa",
-			ExpectedExecutionOutput: expected,
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/all/source.french.swa",
+			expected)
 	})
 }
 
 func TestStructPropertyInReturnExpression(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-		req := CompileRequest{
-			InputPath: "./structs/return-expression/source.english.swa",
-			T:         t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/return-expression/source.english.swa",
+			"Name: Pathe, TechStack: (Ruby, Rust, Go), Age: (40), Height: (1.80)")
 	})
 
 	t.Run("French", func(t *testing.T) {
-		t.Parallel()
-		req := CompileRequest{
-			InputPath: "./structs/return-expression/source.french.swa",
-			T:         t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/return-expression/source.french.swa",
+			"Nom: Pathe, Stack Technique: (Ruby, Rust, Go), Age: (40), Taille: (1.80)")
 	})
 }
 
 func TestNestedStructBasic(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-
-		req := CompileRequest{
-			InputPath:               "./structs/nested/basic/source.english.swa",
-			ExpectedExecutionOutput: "Name: Pathe, City: Dakar, ZipCode: 12000",
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/nested/basic/source.english.swa",
+			"Name: Pathe, City: Dakar, ZipCode: 12000")
 	})
 }
 
 func TestNestedStructInline(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-		req := CompileRequest{
-			InputPath:               "./structs/nested/inline/source.english.swa",
-			ExpectedExecutionOutput: "Name: Sene, City: Thies, ZipCode: 21000",
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/nested/inline/source.english.swa",
+			"Name: Sene, City: Thies, ZipCode: 21000")
 	})
 }
 
 func TestNestedStructFieldAccess(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-		req := CompileRequest{
-			InputPath:               "./structs/nested/field-access/source.english.swa",
-			ExpectedExecutionOutput: "Name: Pathe, Age: 30, City: Dakar, ZipCode: 12000",
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/nested/field-access/source.english.swa",
+			"Name: Pathe, Age: 30, City: Dakar, ZipCode: 12000")
 	})
 }
 
 func TestNestedStructTriple(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-
-		req := CompileRequest{
-			InputPath:               "./structs/nested/triple/source.english.swa",
-			ExpectedExecutionOutput: "X: 10, Y: 20, getX: 10, setX: 999",
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/nested/triple/source.english.swa",
+			"X: 10, Y: 20, getX: 10, setX: 999")
 	})
 }
 
 func TestNestedStructAssignment(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-		req := CompileRequest{
-			InputPath:               "./structs/nested/assignment/source.english.swa",
-			ExpectedExecutionOutput: "City: Dakar, ZipCode: 99999, Code+1: 100000",
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/nested/assignment/source.english.swa",
+			"City: Dakar, ZipCode: 99999, Code+1: 100000")
 	})
 }
 
 func TestNestedStructArrays(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-
-		req := CompileRequest{
-			InputPath:               "./structs/nested/arrays/source.english.swa",
-			ExpectedExecutionOutput: "Person 1 City: Dakar, Person 2 ZipCode: 21000",
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/nested/arrays/source.english.swa",
+			"Person 1 City: Dakar, Person 2 ZipCode: 21000")
 	})
 }
 
 func TestStructArrayEmbedded(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-
-		req := CompileRequest{
-			InputPath:               "./structs/array-embedded/source.english.swa",
-			ExpectedExecutionOutput: "Hello: 100 200 300",
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/array-embedded/source.english.swa",
+			"Hello: 100 200 300")
 	})
 }
 
 func TestStructArrayPointer(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-
-		req := CompileRequest{
-			InputPath:               "./structs/array-pointer/source.english.swa",
-			ExpectedExecutionOutput: "Hello: 100 200 300",
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/array-pointer/source.english.swa",
+			"Hello: 100 200 300")
 	})
 }
 
 func TestStructMultiArrays(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-		req := CompileRequest{
-			InputPath:               "./structs/multi-arrays/source.english.swa",
-			ExpectedExecutionOutput: "arr1[0]=10 arr1[1]=20 arr2[0]=100 arr2[2]=300 value=42",
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/multi-arrays/source.english.swa",
+			"arr1[0]=10 arr1[1]=20 arr2[0]=100 arr2[2]=300 value=42")
 	})
 }
 
 func TestStructMultiPointers(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-
-		req := CompileRequest{
-			InputPath:               "./structs/multi-pointers/source.english.swa",
-			ExpectedExecutionOutput: "ptr1[0]=10 ptr1[1]=20 ptr2[0]=100 ptr2[2]=300 value=99",
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/multi-pointers/source.english.swa",
+			"ptr1[0]=10 ptr1[1]=20 ptr2[0]=100 ptr2[2]=300 value=99")
 	})
 }
 
 func TestStructMixedArrays(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-
-		expected := "embedded[0]=10 embedded[1]=20 pointer[0]=100 pointer[2]=300"
-		req := CompileRequest{
-			InputPath:               "./structs/mixed-arrays/source.english.swa",
-			ExpectedExecutionOutput: expected,
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/mixed-arrays/source.english.swa",
+			"embedded[0]=10 embedded[1]=20 pointer[0]=100 pointer[2]=300")
 	})
 }
 
@@ -332,13 +209,9 @@ func TestStructFuncParamEmbedded(t *testing.T) {
 	t.Parallel()
 
 	t.Run("English", func(t *testing.T) {
-		req := CompileRequest{
-			InputPath:               "./structs/func-param-embedded/source.english.swa",
-			ExpectedExecutionOutput: "Result: 65",
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/func-param-embedded/source.english.swa",
+			"Result: 65")
 	})
 }
 
@@ -346,55 +219,29 @@ func TestStructFuncParamPointer(t *testing.T) {
 	t.Parallel()
 
 	t.Run("English", func(t *testing.T) {
-		req := CompileRequest{
-			InputPath:               "./structs/func-param-pointer/source.english.swa",
-			ExpectedExecutionOutput: "Result: 65",
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/func-param-pointer/source.english.swa",
+			"Result: 65")
 	})
 }
 
 func TestStructWithEmbeddedArrayOfStructs(t *testing.T) {
 	t.Run("English", func(t *testing.T) {
-		t.Parallel()
-
-		req := CompileRequest{
-			InputPath:               "./structs/array-of-structs/source.english.swa",
-			ExpectedExecutionOutput: "p[0].x=1 p[0].y=1\np[1].x=2 p[1].y=2\np[2].x=3 p[2].y=3\n",
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/array-of-structs/source.english.swa",
+			"p[0].x=1 p[0].y=1\np[1].x=2 p[1].y=2\np[2].x=3 p[2].y=3\n")
 	})
 }
 
 func TestStructInitialization(t *testing.T) {
 	t.Run("Init array field with a symbol", func(t *testing.T) {
-		t.Parallel()
-
-		input := "./structs/all/init-field-from-array.swa"
-		expected := "Backing[0]: 10, Wrapper.Data[0]: 10 \nBacking[1]: 20, Wrapper.Data[1]: 20 \nBacking[2]: 30, Wrapper.Data[2]: 30 \nBacking[3]: 0, Wrapper.Data[3]: 0 \nBacking[4]: 0, Wrapper.Data[4]: 0 \nBacking[5]: 0, Wrapper.Data[5]: 0 \nBacking[6]: 0, Wrapper.Data[6]: 0 \nBacking[7]: 0, Wrapper.Data[7]: 0 \nBacking[8]: 0, Wrapper.Data[8]: 0 \nBacking[9]: 0, Wrapper.Data[9]: 0 \n"
-		req := CompileRequest{
-			InputPath:               input,
-			ExpectedExecutionOutput: expected,
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/all/init-field-from-array.swa",
+			"Backing[0]: 10, Wrapper.Data[0]: 10 \nBacking[1]: 20, Wrapper.Data[1]: 20 \nBacking[2]: 30, Wrapper.Data[2]: 30 \nBacking[3]: 0, Wrapper.Data[3]: 0 \nBacking[4]: 0, Wrapper.Data[4]: 0 \nBacking[5]: 0, Wrapper.Data[5]: 0 \nBacking[6]: 0, Wrapper.Data[6]: 0 \nBacking[7]: 0, Wrapper.Data[7]: 0 \nBacking[8]: 0, Wrapper.Data[8]: 0 \nBacking[9]: 0, Wrapper.Data[9]: 0 \n")
 	})
 	t.Run("Init int field with a symbol", func(t *testing.T) {
-		t.Parallel()
-
-		input := "./structs/all/init-field-from-number.swa"
-		expected := "1000"
-		req := CompileRequest{
-			InputPath:               input,
-			ExpectedExecutionOutput: expected,
-			T:                       t,
-		}
-
-		req.AssertCompileAndExecute()
+		NewSuccessfulCompileRequest(t,
+			"./structs/all/init-field-from-number.swa",
+			"1000")
 	})
 }
--- tests/while_test.go
+++ tests/while_test.go
@@ -5,79 +5,71 @@ import (
 )
 
 func TestWhileStatement(t *testing.T) {
-	t.Parallel()
-
 	t.Run("French", func(t *testing.T) {
 		t.Run("Iterate from ten to zero", func(t *testing.T) {
-			req := CompileRequest{
-				InputPath:               "./while/from-ten-to-zero.french.swa",
-				T:                       t,
-				ExpectedExecutionOutput: "10 9 8 7 6 5 4 3 2 1 ",
-			}
-
-			req.AssertCompileAndExecute()
+			NewSuccessfulCompileRequest(t,
+				"./while/from-ten-to-zero.french.swa",
+				"10 9 8 7 6 5 4 3 2 1 ")
 		})
 
 		t.Run("Iterate from zero to ten", func(t *testing.T) {
-			req := CompileRequest{
-				InputPath:               "./while/from-zero-to-ten.french.swa",
-				T:                       t,
-				ExpectedExecutionOutput: "0 1 2 3 4 5 6 7 8 9 ",
-			}
-
-			req.AssertCompileAndExecute()
+			NewSuccessfulCompileRequest(t,
+				"./while/from-zero-to-ten.french.swa",
+				"0 1 2 3 4 5 6 7 8 9 ")
 		})
 
 		t.Run("Iterate from zero to ten included", func(t *testing.T) {
-			req := CompileRequest{
-				InputPath:               "./while/from-zero-to-ten-included.french.swa",
-				T:                       t,
-				ExpectedExecutionOutput: "0 1 2 3 4 5 6 7 8 9 10 ",
-			}
-
-			req.AssertCompileAndExecute()
+			NewSuccessfulCompileRequest(t,
+				"./while/from-zero-to-ten-included.french.swa",
+				"0 1 2 3 4 5 6 7 8 9 10 ")
 		})
 	})
 
 	t.Run("English", func(t *testing.T) {
 		t.Run("Iterate from ten to zero", func(t *testing.T) {
-			req := CompileRequest{
-				InputPath:               "./while/from-ten-to-zero.english.swa",
-				T:                       t,
-				ExpectedExecutionOutput: "10 9 8 7 6 5 4 3 2 1 ",
-			}
-
-			req.AssertCompileAndExecute()
+			NewSuccessfulCompileRequest(t,
+				"./while/from-ten-to-zero.english.swa",
+				"10 9 8 7 6 5 4 3 2 1 ")
 		})
 
 		t.Run("Iterate from zero to ten", func(t *testing.T) {
-			req := CompileRequest{
-				InputPath:               "./while/from-zero-to-ten.english.swa",
-				T:                       t,
-				ExpectedExecutionOutput: "0 1 2 3 4 5 6 7 8 9 ",
-			}
-
-			req.AssertCompileAndExecute()
+			NewSuccessfulCompileRequest(t,
+				"./while/from-zero-to-ten.english.swa",
+				"0 1 2 3 4 5 6 7 8 9 ")
 		})
 
 		t.Run("Iterate array", func(t *testing.T) {
-			req := CompileRequest{
-				InputPath:               "./while/array.english.swa",
-				T:                       t,
-				ExpectedExecutionOutput: "5 15 25 35 45 55 65 75 85 95 ",
-			}
-
-			req.AssertCompileAndExecute()
+			NewSuccessfulCompileRequest(t,
+				"./while/array.english.swa",
+				"5 15 25 35 45 55 65 75 85 95 ")
 		})
 
 		t.Run("Display odd numbers from zero to ten", func(t *testing.T) {
-			req := CompileRequest{
-				InputPath:               "./while/odd-numbers.swa",
-				T:                       t,
-				ExpectedExecutionOutput: "1 3 5 7 9 ",
+			tests := []MultiDialectTest{
+				{
+					name:                    "english",
+					inputPath:               "./while/odd-numbers.swa",
+					expectedExecutionOutput: "1 3 5 7 9 ",
+				},
+				{
+					name:                    "french",
+					inputPath:               "./while/odd-numbers.french.swa",
+					expectedExecutionOutput: "1 3 5 7 9 ",
+				},
+				{
+					name:                    "soussou",
+					inputPath:               "./while/odd-numbers.soussou.swa",
+					expectedExecutionOutput: "1 3 5 7 9 ",
+				},
 			}
 
-			req.AssertCompileAndExecute()
+			for _, test := range tests {
+				t.Run(test.name, func(t *testing.T) {
+					NewSuccessfulCompileRequest(t,
+						test.inputPath,
+						test.expectedExecutionOutput)
+				})
+			}
 		})
 
 		t.Run("Display even numbers from zero to ten", func(t *testing.T) {
@@ -96,7 +88,16 @@ func TestWhileStatement(t *testing.T) {
 				ExpectedExecutionOutput: "0 1 2 3 4 5 6 7 8 9 10 ",
 			}
 
-			req.AssertCompileAndExecute()
+		t.Run("Display even numbers from zero to ten", func(t *testing.T) {
+			NewSuccessfulCompileRequest(t,
+				"./while/even-numbers.swa",
+				"0 2 4 6 8 ")
+		})
+
+		t.Run("Iterate from zero to ten included", func(t *testing.T) {
+			NewSuccessfulCompileRequest(t,
+				"./while/from-zero-to-ten-included.english.swa",
+				"0 1 2 3 4 5 6 7 8 9 10 ")
 		})
 	})
 }
