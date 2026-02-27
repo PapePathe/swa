54dbcbfa8223b8594425b381f07b7995fcd055af
pathe <pathe.sene@gmail.com>
2026-02-27 20:50:37 +0000

    bugfix: allocate correct type for int64

--- lang/compiler/llvm_generator_variable_declaration.go
+++ lang/compiler/llvm_generator_variable_declaration.go
@@ -143,7 +143,7 @@ func (g *LLVMGenerator) declareVarWithInitializer(node *ast.VarDeclarationStatem
 			}
 
 			lastres := g.getLastResult()
-			alloc := g.Ctx.Builder.CreateAlloca(res.Value.Type(), node.Name)
+			alloc := g.Ctx.Builder.CreateAlloca(lastres.Value.Type(), node.Name)
 			g.Ctx.Builder.CreateStore(*lastres.Value, alloc)
 			finalAddr = &alloc
 		case ast.DataTypeNumber:
