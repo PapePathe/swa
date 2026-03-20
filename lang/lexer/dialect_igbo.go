2d62562b19bc7f4b5878607d4978d117ada056ac
pathe <pathe.sene@gmail.com>
2026-03-20 15:09:09 +0000

    feat(dialect-igbo): use keywords from innocent

--- lang/lexer/dialect_igbo.go
+++ lang/lexer/dialect_igbo.go
@@ -4,16 +4,15 @@ import (
 	"regexp"
 )
 
+// Igbo
+//
 // Country: Nigeria
-// Igbo is a major Nigerian language spoken primarily in the South-Eastern region by the
-// Igbo people. It is a tonal language in the Benue–Congo branch of the Niger-Congo family
-// and is central to the cultural and commercial identity of Igboland.
 type Igbo struct{}
 
 var _ Dialect = (*Igbo)(nil)
 
 func (m Igbo) DetectionPattern() *regexp.Regexp {
-	return regexp.MustCompile(`igbo:igbo;`)
+	return regexp.MustCompile(`oluasusu:igbo;`)
 }
 
 func (m Igbo) Name() string {
@@ -22,25 +21,28 @@ func (m Igbo) Name() string {
 
 func (m Igbo) Reserved() map[string]TokenKind {
 	return map[string]TokenKind{
-		"ka":       Let,
-		"kwụm":     Const,
-		"ọbụrụ":    KeywordIf,
-		"ọbụrụ-na": KeywordElse,
-		"mgbe":     KeywordWhile,
-		"ọrụ":      Function,
-		"lọghachi": Return,
-		"mbido":    Main,
-		"pụta":     Print,
-		"nhazi":    Struct,
-		"ezigbo":   True,
-		"ụgha":     False,
-		"bool":     TypeBool,
-		"byte":     TypeByte,
-		"desimal":  TypeFloat,
-		"nọmba":    TypeInt,
-		"nọmba64":  TypeInt64,
-		"okwu":     TypeString,
-		"njehie":   TypeError,
+		"oluasusu":   DialectDeclaration,
+		"ka":         Let,
+		"ogidi":      Const,
+		"maobu":      KeywordElse,
+		"oburu":      KeywordIf,
+		"dika":       KeywordWhile,
+		"oru":        Function,
+		"weghachi":   Return,
+		"mbido":      Main,
+		"deputa":     Print,
+		"nhazi":      Struct,
+		"ezigbo":     True,
+		"ụgha":       False,
+		"bool":       TypeBool,
+		"byte":       TypeByte,
+		"ofe":        TypeFloat,
+		"onuogugu":   TypeInt,
+		"onuogugu64": TypeInt64,
+		"ederede":    TypeString,
+		"njehie":     TypeError,
+		"ogologo":    Variadic,
+		"efu":        Zero,
 	}
 }
 
