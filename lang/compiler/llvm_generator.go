1561f74c8f7f1af26f3324b692e3e458840edde1
pathe <pathe.sene@gmail.com>
2026-04-19 02:06:50 +0000

    chore: remove blank line

--- lang/compiler/llvm_generator.go
+++ lang/compiler/llvm_generator.go
@@ -211,6 +211,7 @@ func (g *LLVMGenerator) VisitFloatExpression(node *ast.FloatExpression) error {
 }
 
 const MAX_PARAMS_SIZE = 1000
+
 func (g *LLVMGenerator) VisitMainStatement(node *ast.MainStatement) error {
 	// FIXME we may not need this function anymore
 	g.Ctx.IncrementMainOccurrences()
